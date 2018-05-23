b70b48a603c14cea99e39b6bef3bef283a287453
DESKTOP-1EU3016\windows <vandoanh94@gmail.com>
2018-05-23 22:55:50 +0700

    done easy notification .. not sound, not buzzzz

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {Platform} from 'ionic-angular'
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(private localNotifications:LocalNotifications, private platform:Platform) {
 
+  }
+
+  btnPushClick(){
+    this.platform.ready().then(() => {
+      this.localNotifications.schedule({
+        title:'THIS IS MY NOTIFICATION',
+        text: 'Delayed ILocalNotification',
+        trigger: {at: new Date(new Date().getTime() + 3600)},
+        led: 'FF0000',
+        icon: 'http://icons.iconarchive.com/icons/paomedia/small-n-flat/24/bell-icon.png',
+        sound: null
+     });
+    });
   }
 }
